ac45590ed8150f80b62a87292e7d95a6ed657e99
hkjohn2001 <hkjohn2001@gmail.com>
2011-01-05 20:22:49 -0700

    Improved the user show page and added a Gravatar

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,10 +14,26 @@ describe UsersController do
       response.should be_success 
     end 
     
-    it "should find teh right user" do 
+    it "should find the right user" do 
       get :show, :id => @user
       assigns(:user).should == @user 
     end 
+    
+    it "should have the right title" do 
+      get :show, :id => @user
+      response.should have_selector('title', :content => @user.name)
+    end  
+    
+     it "should have the user's name" do 
+        get :show, :id => @user
+        response.should have_selector('h1', :content => @user.name)
+     end
+     
+     it "should have a profile image" do 
+       get :show, :id => @user
+       response.should have_selector('h1>img', :class => "gravatar")
+     end 
+    
   end 
 
   describe "GET 'new'" do
