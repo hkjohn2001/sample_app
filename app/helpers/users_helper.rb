36b2f004f666780ee8ce7a9f5f48b2c1801ba2af
hkjohn2001 <hkjohn2001@gmail.com>
2011-01-06 20:56:37 -0700

    Added a sidebar and some styling

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   
-  def gravatar_for(user, options = { :size => 50}) 
+  def gravatar_for(user, options = { :size => 10, :alt => user.name}) 
     gravatar_image_tag(user.email.downcase, :alt => user.name, 
                                             :class => 'gravatar', 
                                             :gravator => options)
