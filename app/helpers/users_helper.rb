6824edf022c3e06e45c86e9c8b375e91d4bf4b4e
hkjohn2001 <hkjohn2001@gmail.com>
2011-01-22 18:24:28 -0700

    Pages for following/follwers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
   def gravatar_for(user, options = { :size => 10, :alt => user.name}) 
     gravatar_image_tag(user.email.downcase, :alt => user.name, 
                                             :class => 'gravatar', 
-                                            :gravator => options)
+                                            :gravator => options )
   end 
   
 end
